0cd91f518171d36a710ddeefc0b10ceb574eb528
qualname <mhegyi@pm.me>
2019-09-16 18:23:04 +0100

    the very first push button!

--- src/buttons.py
+++ src/buttons.py
@@ -3,8 +3,16 @@ import cv2
 from utils import *
 
 
+@update_global_image_var
+@display_returned_image
+def load_image(*_):
+    return cv2.imread('resources/test.jpeg', cv2.IMREAD_COLOR)
+
+
 # 'buttonType' values:
 #     one of: QT_CHECKBOX / QT_PUSH_BUTTON / QT_RADIOBOX
 #     QT_NEW_BUTTONBAR is kinda like a linebreak
 
-BUTTONS = {}
+BUTTONS = {
+    'Load image': {'onChange': load_image},
+}
