1f49b28a501afe9ee747a699d417d015c0fb7d75
qualname <mhegyi@pm.me>
2019-09-16 18:50:41 +0100

    sample checkbox button

--- src/buttons.py
+++ src/buttons.py
@@ -3,10 +3,23 @@ import cv2
 from utils import *
 
 
+UNCHECKED = 0
+CHECKED = 1
+
+
 @update_global_image_var
 @display_returned_image
 def load_image(*_):
     return cv2.imread('resources/test.jpeg', cv2.IMREAD_COLOR)
+
+
+@update_global_image_var
+@display_returned_image
+def toggle_hsv(img, btn_state, *_):
+    if btn_state == CHECKED:
+        return cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    else:
+        return cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
 
 
 # 'buttonType' values:
@@ -15,4 +28,5 @@ def load_image(*_):
 
 BUTTONS = {
     'Load image': {'onChange': load_image},
+    'Toggle HSV': {'onChange': toggle_hsv, 'buttonType': cv2.QT_CHECKBOX},
 }
